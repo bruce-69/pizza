98c7424828a72dc62932f2c02541cc57f48d3fd3
jorge <eduardcrow555@gmail.com>
2023-03-09 14:31:12 -0300

    preço

--- app/src/components/OrderDetails/index.tsx
+++ app/src/components/OrderDetails/index.tsx
@@ -1,40 +1,72 @@
-//nao sei se estar certo acho esse codigo é no order details
-import { ReactComponent as Search } from "assets/icons/search.svg";
+import ButtonLarge from "components/ButtonLarge";
+import ButtonToggle from "components/ButtonToggle";
+import OrderItem from "components/OrderItem";
+import OrderItemList from "components/OrderItemList";
+import { HTMLAttributes, useEffect, useState } from "react";
+import { OrderItemType } from "types/OrderItemType";
 import * as S from "./style";
 
-const Home = () => {
+type OrderDetailsType = HTMLAttributes<HTMLDivElement>;
+
+type OrderDetailsProps = {
+  orders: OrderItemType[];
+} & OrderDetailsType;
+
+const OrderDetails = ({ orders }: OrderDetailsProps) => {
+  const price = orders
+    .map((i) => i.product.price * i.quantity)
+    .reduce((a, b) => a + b, 0);
+
+  const [priceState, setPriceState] = useState(price);
+
+  useEffect(() => {
+    setPriceState(price);
+  }, [orders, price]);
+
   return (
-    <S.Home>
-      <S.HomeContent>
-        <header>
-          <S.HomeHeaderDetails>
-            <div>
-              <S.HomeHeaderDetailsLogo>Pizza Fresh</S.HomeHeaderDetailsLogo>
-              <S.HomeHeaderDetailsDate>
-                Aqui ficará a data
-              </S.HomeHeaderDetailsDate>
-            </div>
-            <S.HomeHeaderDetailsSearch>
-              <Search />
-              <input type="text" placeholder="Procure pelo sabor" />
-            </S.HomeHeaderDetailsSearch>
-          </S.HomeHeaderDetails>
-        </header>
-        <div>
-          <S.HomeProductTitle>
-            <b>Pizzas</b>
-          </S.HomeProductTitle>
-          <S.HomeProductList>
-            <p>Lista de produtos aqui</p>
-          </S.HomeProductList>
-        </div>
-      </S.HomeContent>
-      <aside>
-        <p>Detalhes dos pedidos aqui</p>
-      </aside>
-    </S.Home>
+    <S.OrderDetails>
+      <S.OrderDetailsTitle>Detalhes do Pedido</S.OrderDetailsTitle>
+      <S.OrderDetailsButtonGroup>
+        <ButtonToggle active={true} value="Comer no Local" />
+        <ButtonToggle active={false} value="P/ Viagem" />
+        <ButtonToggle active={true} value="Delivery" />
+      </S.OrderDetailsButtonGroup>
+      <S.OrderDetailsList>
+        <OrderItemList
+          header={
+            <S.OrderDetailsListTitle>
+              <h4>Item</h4>
+              <h4>Qtd</h4>
+              <h4>Preço</h4>
+            </S.OrderDetailsListTitle>
+          }
+          list={
+            Boolean(orders.length) ? (
+              orders.map((item, index) => (
+                <OrderItem
+                  product={item.product}
+                  quantity={item.quantity}
+                  observation={item.observation}
+                  key={`OrderDetails-${index}`}
+                />
+              ))
+            ) : (
+              <S.OrderDetailsListGap />
+            )
+          }
+          footer={
+            <S.OrderDetailsListFooter>
+              <S.OrderDetailsListFooterRow>
+                <span>Subtotal</span>
+                <span>R$ {priceState.toFixed(2)}</span>
+              </S.OrderDetailsListFooterRow>
+              <ButtonLarge value="Continue para o pagamento" />
+            </S.OrderDetailsListFooter>
+          }
+        />
+      </S.OrderDetailsList>
+    </S.OrderDetails>
   );
 };
 
-export default Home;
-
+export default OrderDetails;
